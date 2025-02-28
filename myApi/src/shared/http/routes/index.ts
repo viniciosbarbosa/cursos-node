e1de1d590232ad29d7c50fa36cbe2d01c15b1c4c
viniciosl <viniciosbarbosagit@gmail.com>
2025-02-28 01:33:44 -0300

    add erros middleware

--- myApi/src/shared/http/routes/index.ts
+++ myApi/src/shared/http/routes/index.ts
@@ -1,8 +1,10 @@
+import { AppError } from "@shared/errors/AppError";
 import { Router } from "express";
 
 const routes = Router();
 
-routes.get("/", (request, response) => {
+routes.get("/", (error, request, response) => {
+  throw new AppError("Acesso negado");
   return response.json({ message: "Olá, dev" });
 });
 
