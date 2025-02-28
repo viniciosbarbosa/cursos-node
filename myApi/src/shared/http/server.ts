14ae8537dad5b0e7ffdbc41e749f4e788d7161a5
viniciosl <viniciosbarbosagit@gmail.com>
2025-02-28 00:59:51 -0300

    add libs cors and express

--- myApi/src/shared/http/server.ts
+++ myApi/src/shared/http/server.ts
@@ -1,7 +1,16 @@
-export class AppServer {
-  private app: string;
+import express from "express";
+import "express-async-errors";
+import cors from "cors";
 
-  constructor(info: string) {
-    this.app = info ? "Ola dev" : "";
-  }
-}
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (request, response) => {
+  return response.json({ message: "Olá, dev" });
+});
+
+app.listen(3000, () => {
+  console.log("Servidor rodando na porta 3000 🚀");
+});
